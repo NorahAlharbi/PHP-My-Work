66350353c4d155dab10ed9561446b327a1098867
Norah Alharbi <nalharbi@waqood.sa>
2020-02-23 13:54:02 +0300

    test-two

--- Summary.php
+++ Summary.php
@@ -357,8 +357,52 @@ function sum($x, $y) {
 echo "5 + 10 = " . sum(5, 10) . "<br>";
 echo "7 + 13 = " . sum(7, 13) . "<br>";
 echo "2 + 4 = " . sum(2, 4);
+echo "<br>";
 
+//An array stores multiple values in one single variable:
+//In PHP, there are three types of arrays:
+//Indexed arrays - Arrays with a numeric index.
+//Associative arrays - Arrays with named keys.
+//Multidimensional arrays - Arrays containing one or more arrays.
 
+//Indexed Arrays
+$car = array("Volvo", "BMW", "Toyota");
+echo "I like " . $car[0] . ", " . $car[1] . " and " . $car[2] . ".";
+echo "<br>";
+
+//Associative Arrays
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+echo "Peter is " . $age['Peter'] . " years old.";
+echo "<br>";
+
+foreach($age as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}
+
+//Multidimensional Arrays: A multidimensional array is an array containing one or more arrays.
+$cars = array
+(
+    array("Volvo",22,18),
+    array("BMW",15,13),
+    array("Saab",5,2),
+    array("Land Rover",17,15)
+);
+
+echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
+echo "<br>";
+
+for ($row = 0; $row < 4; $row++) {
+    echo "<p><b>Row number $row</b></p>";
+    echo "<ul>";
+    for ($col = 0; $col < 3; $col++) {
+        echo "<li>".$cars[$row][$col]."</li>";
+    }
+    echo "</ul>";
+}
 ?>
 
 
