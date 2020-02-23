bc530bdf4554bc7f8b024ab5218cfc131ff79b04
Norah Alharbi <nalharbi@waqood.sa>
2020-02-23 11:56:39 +0300

    test-two

--- Summary.php
+++ Summary.php
@@ -7,6 +7,10 @@
 </head>
 
 <body>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    <input type="number" name="price" placeholder="Price">
+    <input type="submit" name="submit" value="submit">
+</form>
 
 <?php
 //A Variables are "containers" for storing information and in variables the value can be assigned and changed at any time without issue.
@@ -177,7 +181,7 @@ echo "<br>";
 //The if statement executes some code if one condition is true
 $t = date("h:i:sa");
 
-if ($t < "17") {
+if ($t < "20") {
     echo "Have a good day!";
 }
 echo "<br>";
@@ -205,12 +209,158 @@ if ($t < "10") {
 echo "<br>";
 
 //Ternary operator .. Syntax: (Condition) ? (Statement1) : (Statement2);
-$score = 10;
-$age = 20;
-echo 'Taking into account your age and score, you are: ',($age > 10 ? ($score < 80 ? 'behind' : 'above average') : ($score < 50 ? 'behind' : 'above average'));
+$laptop = 1500;
+$camera = 300;
+$mobile = 200;
+
+echo ($laptop < $camera) ? ("I want buy the camera!") : ("I want buy the mobile!");
+echo "<br>";
+
+//The switch statement is used to perform different actions based on different conditions.
+$favoritecolor = "red";
+
+switch ($favoritecolor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+}
+echo "<br>";
+
+//The while loop executes a block of code as long as the specified condition is true.
+$x = 1;
+while($x <= 5) {
+    echo "The number is: $x <br>";
+    $x++;
+}
+
+$y = 0;
+while($y <= 100) {
+    echo "The number is: $y <br>";
+    $y+=10;
+}
+
+//The do...while loop will always execute the block of code once, it will then check the condition, and repeat the loop while the specified condition is true.
+$d = 1;
+do {
+    echo "The number is: $d <br>";
+    $d++;
+} while ($d <= 5);
+
+//Note: In a do...while loop the condition is tested AFTER executing the statements within the loop. This means that the do...while loop will execute its statements at least once, even if the condition is false. See example below.
+$z = 6;
+do {
+    echo "The number is: $z <br>";
+    $z++;
+} while ($z <= 5);
+
+
+//The for loop - Loops through a block of code a specified number of times.
+//The for loop is used when you know in advance how many times the script should run.
+for ($i = 0; $i <= 10; $i++) {
+    echo "The number is: $i <br>";
+}
+
+for ($j = 0; $j <= 100; $j+=10) {
+    echo "The number is: $j <br>";
+}
+
+//PHP User Defined Functions:
+//Besides the built-in PHP functions, it is possible to create your own functions.
+//A function is a block of statements that can be used repeatedly in a program.
+//A function will not execute automatically when a page loads.
+//A function will be executed by a call to the function.
+//A function name must start with a letter or an underscore. Function names are NOT case-sensitive.
+function writeMsg() {
+    echo "Hello world!";
+}
+
+writeMsg(); // call the function
+echo "<br>";
+
+function _writeMsg() {
+    return "Hello world!";
+}
+
+echo _writeMsg(); // call the function
+echo "<br>";
+
+//PHP Function Arguments:
+//Information can be passed to functions through arguments. An argument is just like a variable.
+//Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.
+//The following example has a function with one argument ($fname). When the familyName() function is called, we also pass along a name (e.g. Jani), and the name is used inside the function, which outputs several different first names, but an equal last name:
+
+function familyName($fname) {
+    echo "$fname Alharbi.<br>";
+}
+
+familyName("Norah");
+familyName("Yousef");
+familyName("Yassmen");
+familyName("Rawabi");
+familyName("Yasser");
+
+
+function studentInformation($fname, $year) {
+    echo "$fname Alhrbi. Born in $year <br>";
+}
+
+studentInformation("Norah", "1975");
+studentInformation("Rawabi", "1978");
+studentInformation("Yasser", "1983");
+
+
+function total() {
+    $price = 60;
+    $halfOfPrice = $price/10;
+    $halfOfHalfOfPrice = $halfOfPrice/2;
+    $percent = $halfOfHalfOfPrice*3;
+
+   echo "The total is: " .$total = $price + $percent;
+}
+total();
+echo "<br>";
+
+    if(isset($_POST['submit']))
+    {
+        $price = $_POST['price'];
+        $halfOfPrice = $price/10;
+        $halfOfHalfOfPrice = $halfOfPrice/2;
+        $percent = $halfOfHalfOfPrice*3;
+        $tax = $price + $percent;
+        echo "The price with tax is: ".$tax;
+    }
+echo "<br>";
+
+
+//The following example shows how to use a default parameter. If we call the function setHeight() without arguments it takes the default value as argument:
+function setHeight($minheight = 50) {
+    echo "The height is : $minheight <br>";
+}
+setHeight(350);
+setHeight(); // will use the default value of 50
+setHeight(135);
+setHeight(80);
+
+//To let a function return a value, use the return statement:
+function sum($x, $y) {
+    $z = $x + $y;
+    return $z;
+}
+echo "5 + 10 = " . sum(5, 10) . "<br>";
+echo "7 + 13 = " . sum(7, 13) . "<br>";
+echo "2 + 4 = " . sum(2, 4);
 
 
 ?>
+
 
 
 
